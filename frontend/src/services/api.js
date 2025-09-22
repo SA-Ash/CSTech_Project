65765f566de701eb52eba53d03156ef740f8584a
SA-Ash <sai.ashrith2605@gmail.com>
2025-09-22 21:15:06 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import dotenv from "dotenv"
-dotenv.config()
+
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.BACKEND_URL}/api`,
+    baseUrl: `${import.meta.env.BACKEND_URL}/api`,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
