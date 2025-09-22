b13da68f6d6944e047cd74d805ef2cb95d37ed3f
SA-Ash <sai.ashrith2605@gmail.com>
2025-09-22 21:38:06 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.BACKEND_URL}/api`,
+    baseUrl: 'http://localhost:5000/api',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) {
